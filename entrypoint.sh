2fc3079baaf6bf74b120f7fcf65988948142b8c5
Velez Ezequiel <ezequiel_velez@mcafee.com>
2020-05-21 09:24:23 -0300

    remove invalid parameter

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@
 FILTERLOCATION=$1
 CODEFOLDERS=$2
 
-report=$(python3 /nsiqcppstyle/nsiqcppstyle.py --output csv -f "$FILTERLOCATION" "$CODEFOLDERS")
+report=$(python3 /nsiqcppstyle/nsiqcppstyle.py -f "$FILTERLOCATION" "$CODEFOLDERS")
 
 echo ::set-output name=report::$report
